65e62c53911b5cc58af697c94c7cf45e7726d5c9
Buhle5969 <cimeleesethu@gmail.com>
2024-08-13 22:18:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function addToCart(course, price) {
     cart.push({ course, price });
     alert(`${course} added to cart.`);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cartItems = document.getElementById('cartItems');
+    const totalElem = document.getElementById('total');
+    const checkoutBtn = document.getElementById('checkoutBtn');
